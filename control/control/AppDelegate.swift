b664a563f090a2e8fa9e0c76e51d2ea74c3ba1f3
Amanda Campos <Amanda@amanda.local>
2016-03-31 13:54:02 -0300

    ícones!

--- control/control/AppDelegate.swift
+++ control/control/AppDelegate.swift
@@ -49,7 +49,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, WCSessionDelegate {
     lazy var applicationDocumentsDirectory: NSURL = {
         // The directory the application uses to store the Core Data store file. This code uses a directory named "Bepid.MC03" in the application's documents Application Support directory.
         let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
-        return urls[urls.count-1] as! NSURL
+        return urls[urls.count-1] 
     }()
     
     lazy var managedObjectModel: NSManagedObjectModel = {
